a214dde992af25d43628d8c2ef4a14bdf5563d26
Artem Ulko <ulkoart@gmail.com>
2020-09-14 10:29:58 +0300

    add image to push

--- Notifications/Notifications.swift
+++ Notifications/Notifications.swift
@@ -14,85 +14,101 @@ class Notifications: NSObject, UNUserNotificationCenterDelegate {
     let notificationsCenter = UNUserNotificationCenter.current()
     
     func requestAutorization() {
-          notificationsCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
-              print("Prmission granted: \(granted)")
-              
-              guard granted else { return }
-              self.getNotificationSettings()
-          }
-      }
-      
-      func getNotificationSettings() {
-          notificationsCenter.getNotificationSettings { (settings) in
-              print(settings)
-          }
-      }
-      
-      func scheduleNotification(notificationType: String) {
-          
-          let content = UNMutableNotificationContent()
-          let userAction = "User Action"
-          
-          content.title = notificationType
-          content.body = "This is example how to create"
-          content.sound = UNNotificationSound.default
-          content.badge = 1
-          content.categoryIdentifier = userAction
-          
-          let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-          
-          let identifire = "Local Notification"
-          let request = UNNotificationRequest(identifier: identifire, content: content, trigger: trigger)
-          
-          notificationsCenter.add(request) { (error) in
-              if let error = error {
-                  print("Error \(error.localizedDescription)")
-              }
-          }
-          
-          
-          let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
-          let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
-          
-          let category = UNNotificationCategory(
-              identifier: userAction,
-              actions: [snoozeAction, deleteAction],
-              intentIdentifiers: [],
-              options: [])
-          
-          notificationsCenter.setNotificationCategories([category])
-          
-          
-          
-      }
+        notificationsCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Prmission granted: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        notificationsCenter.getNotificationSettings { (settings) in
+            print(settings)
+        }
+    }
+    
+    func scheduleNotification(notificationType: String) {
+        
+        let content = UNMutableNotificationContent()
+        let userAction = "User Action"
+        
+        content.title = notificationType
+        content.body = "This is example how to create"
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        content.categoryIdentifier = userAction
+        
+        guard let path = Bundle.main.path(forResource: "favicon", ofType: "png") else { return }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let attachment = try UNNotificationAttachment(
+                identifier: "favicon",
+                url: url,
+                options: nil
+            )
+            content.attachments = [attachment]
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let identifire = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifire, content: content, trigger: trigger)
+        
+        notificationsCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+        
+        
+        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
+        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
+        
+        let category = UNNotificationCategory(
+            identifier: userAction,
+            actions: [snoozeAction, deleteAction],
+            intentIdentifiers: [],
+            options: [])
+        
+        notificationsCenter.setNotificationCategories([category])
+        
+        
+        
+    }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-           completionHandler([.alert, .sound])
-       }
-       
-       func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-           
-           print(response.notification.request.identifier)
-           
-           if response.notification.request.identifier == "Local Notification" {
-               print("Handling notification with the Local Notofocation Identifire")
-           }
-           
-           switch response.actionIdentifier {
-           case UNNotificationDismissActionIdentifier:
-               print("Dismis Action")
-           case UNNotificationDefaultActionIdentifier:
-               print("Default")
-           case "Snooze":
-               print("Snooze")
-               scheduleNotification(notificationType: "Reminder")
-           case "Delete":
-               print("Delete")
-           default:
-               fatalError()
-           }
-           
-           completionHandler()
-       }
-
+        completionHandler([.alert, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print(response.notification.request.identifier)
+        
+        if response.notification.request.identifier == "Local Notification" {
+            print("Handling notification with the Local Notofocation Identifire")
+        }
+        
+        switch response.actionIdentifier {
+        case UNNotificationDismissActionIdentifier:
+            print("Dismis Action")
+        case UNNotificationDefaultActionIdentifier:
+            print("Default")
+        case "Snooze":
+            print("Snooze")
+            scheduleNotification(notificationType: "Reminder")
+        case "Delete":
+            print("Delete")
+        default:
+            fatalError()
+        }
+        
+        completionHandler()
+    }
+    
 }
